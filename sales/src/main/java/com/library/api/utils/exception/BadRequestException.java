06e95727c3e4f03e25c8969d2de6271393c3f433
Gbeminiyi Ariyo-Agbaje <gbeminiyi.ariyo@longbridgetech.com>
2022-02-10 09:05:51 +0100

    gbeminiyi

--- sales/src/main/java/com/library/api/utils/exception/BadRequestException.java
+++
@@ -1,8 +0,0 @@
-package com.library.api.utils.exception;
-
-public class BadRequestException extends AbstractException {
-
-    public BadRequestException(String code, String message) {
-        super(code, message);
-    }
-}
